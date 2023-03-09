61f482fac4235dc58adb02117b17f9000a864884
Tamás Russ <t.russ@sprinteins.com>
2023-03-09 16:41:26 +0100

    ensure branch

--- infra/create-build-branch.sh
+++ infra/create-build-branch.sh
@@ -2,7 +2,22 @@
 branch_name=$1
 
 echo creating build branch at ${branch_name}
-git checkout --orphan "${branch_name}"
+
+echo git ls-remote --exit-code --heads origin ${branch_name}
+git ls-remote --exit-code --heads origin ${branch_name}
+has_branch=$?
+echo has_branch=$has_branch
+
+if [ $has_branch = "2" ]; then
+	echo "no branch, creating"
+	git checkout --orphan "${branch_name}"
+else
+	echo "has branch, checking out"
+	git checkout -b ${branch_name} origin/${branch_name}
+fi
+
+
+exit 0
 git rm -rf .
 git commit --allow-empty -m "init"
-git push -u origin "'${branch_name}'"
+#git push -u origin "'${branch_name}'"
