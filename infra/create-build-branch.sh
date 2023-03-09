df8cd7ee8f9dc2bc8f50b4bd0c7adce5dc1d1bd2
Tamás Russ <t.russ@sprinteins.com>
2023-03-09 14:39:51 +0100

    fix: typo

--- infra/create-build-branch.sh
+++ infra/create-build-branch.sh
@@ -4,5 +4,5 @@ branch_name=$1
 echo creating build branch at ${branch_name}
 echo git checkout --orphan "${branch_name}"
 echo git rm -rf .
-echo git commit --alow-empty -m "init"
+echo git commit --allow-empty -m "init"
 echo git push origin "'${branch_name}'"
