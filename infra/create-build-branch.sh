962c41a6b4e7b8cb0111d00a4b7745eb30aa891f
Tamás Russ <t.russ@sprinteins.com>
2023-03-09 16:50:02 +0100

    activate everything

--- infra/create-build-branch.sh
+++ infra/create-build-branch.sh
@@ -21,8 +21,8 @@ if [ $? -gt 0 ]; then
 	exit $?
 fi
 
-# git rm -rf .
-# git commit --allow-empty -m "init"
+git rm -rf . && \
+git commit --allow-empty -m "init"
 
-#git push -u origin "'${branch_name}'"
+git push -u origin "'${branch_name}'"
 git checkout $current_branch 
