ac6e467ba6d96f861dd1f5182ecdfe7abeeaef16
Tamás Russ <t.russ@sprinteins.com>
2023-03-09 14:53:23 +0100

    fix push

--- infra/create-build-branch.sh
+++ infra/create-build-branch.sh
@@ -5,4 +5,4 @@ echo creating build branch at ${branch_name}
 git checkout --orphan "${branch_name}"
 git rm -rf .
 git commit --allow-empty -m "init"
-git push origin "'${branch_name}'"
+git push -u origin "'${branch_name}'"
