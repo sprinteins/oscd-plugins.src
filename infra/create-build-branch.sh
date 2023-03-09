1084107f56a713219435221cf52971288d38bf9e
Tamás Russ <t.russ@sprinteins.com>
2023-03-09 14:32:55 +0100

    build branch creator

--- infra/create-build-branch.sh
+++ infra/create-build-branch.sh
@@ -2,3 +2,7 @@
 branch_name=$1
 
 echo creating build branch at ${branch_name}
+echo git checkout --orphan "'${branch_name}'"
+echo git rm -rf .
+echo git commit --alow-empty -m "init"
+echo git push origin "'${branch_name}'"
