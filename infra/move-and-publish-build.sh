c4cd1be5b742573fb98b9547f1dbe7b4ae2a15ab
Tamás Russ <t.russ@sprinteins.com>
2023-03-09 17:20:52 +0100

    disable

--- infra/move-and-publish-build.sh
+++ infra/move-and-publish-build.sh
@@ -6,12 +6,14 @@ current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 echo moving and commiting at $output_path
 
+git fetch
 git switch -C ${branch_name} origin/${branch_name}
+git pull
 
 rm -rf ./dist
 mv -v -f ./$output_path/dist ./ && \
-git add --force ./dist && \
-git commit -m "update plugin" && \
-git push origin
-
-git checkout $current_branch 
+# git add --force ./dist && \
+# git commit -m "update plugin" && \
+# git push origin
+# 
+# git checkout $current_branch 
