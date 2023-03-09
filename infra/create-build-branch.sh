04f9b8f0d725562e3302ea7e90f31c5273c4016f
Tamás Russ <t.russ@sprinteins.com>
2023-03-09 16:49:05 +0100

    switch back to current branch

--- infra/create-build-branch.sh
+++ infra/create-build-branch.sh
@@ -3,21 +3,26 @@ branch_name=$1
 
 echo creating build branch at ${branch_name}
 
-echo git ls-remote --exit-code --heads origin ${branch_name}
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
 git ls-remote --exit-code --heads origin ${branch_name}
 has_branch=$?
-echo has_branch=$has_branch
 
 if [ $has_branch = "2" ]; then
 	echo "no branch, creating"
-	git checkout --orphan "${branch_name}"
+	git switch --orphan "${branch_name}"
 else
 	echo "has branch, checking out"
-	git checkout -b ${branch_name} origin/${branch_name}
+	git switch -C ${branch_name} origin/${branch_name}
 fi
 
+if [ $? -gt 0 ]; then
+	echo "something went wrong, exiting"
+	exit $?
+fi
 
-exit 0
-git rm -rf .
-git commit --allow-empty -m "init"
+# git rm -rf .
+# git commit --allow-empty -m "init"
+
 #git push -u origin "'${branch_name}'"
+git checkout $current_branch 
