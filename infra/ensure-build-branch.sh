fd4bf41561a81942ebd4cfdfa65e31a3fd195363
Tamás Russ <t.russ@sprinteins.com>
2023-03-09 16:59:04 +0100

    ensure branch

--- infra/ensure-build-branch.sh
+++ infra/ensure-build-branch.sh
@@ -1,19 +1,20 @@
 
 branch_name=$1
 
-echo creating build branch at ${branch_name}
+echo ensuring build branch at ${branch_name}
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-git ls-remote --exit-code --heads origin ${branch_name}
+git ls-remote --exit-code --heads origin ${branch_name} > /dev/null
 has_branch=$?
 
 if [ $has_branch = "2" ]; then
 	echo "no branch, creating"
 	git switch --orphan "${branch_name}"
 else
-	echo "has branch, checking out"
-	git switch -C ${branch_name} origin/${branch_name}
+	echo "has branch, exiting"
+	exit 0
+	# git switch -C ${branch_name} origin/${branch_name}
 fi
 
 if [ $? -gt 0 ]; then
