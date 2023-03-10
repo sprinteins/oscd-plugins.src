a29f8e0b2e6f1eefa6c1b5efc0e91ed81052ad0d
Tamás Russ <t.russ@sprinteins.com>
2023-03-10 16:46:57 +0100

    build script

--- infra/move-and-publish-build.sh
+++ infra/move-and-publish-build.sh
@@ -18,6 +18,9 @@ if [ $? -gt 0 ]; then
 fi
 
 rm -rf ./dist && \
+git checkout $current_branch -- ./$plugin_root/dist && \
+git checkout $current_branch -- ./$plugin_root/package.json && \
+git reset && \
 mv -v -f ./$plugin_root/dist ./ && \
 mv -v -f ./$plugin_root/package.json ./ && \
 git add --force ./dist && \
