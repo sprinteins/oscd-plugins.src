1eb2bb7cbd39c079f07dd7a7a556de6f5bb911f9
Tamás Russ <t.russ@sprinteins.com>
2023-04-26 16:28:57 +0200

    feat: set message color

--- packages/uilib/src/lib/components/diagram/message.svelte
+++ packages/uilib/src/lib/components/diagram/message.svelte
@@ -17,9 +17,9 @@
 
 	const defaultColor = "var(--color-black)"
 	const messageTypeToColorMap: {[key in MessageType]: string} = {
-		[MessageType.GOOSe]:         "var(--color-green)",
-		[MessageType.MMS]:	         defaultColor,
-		[MessageType.SampledValues]: defaultColor,
+		[MessageType.GOOSe]:         "var(--color-message-goose)",
+		[MessageType.MMS]:	         "var(--color-message-mms)",
+		[MessageType.SampledValues]: "var(--color-message-sampledvalues)",
 	}
 
 	$: pathColor = calcPathColor(edge)
