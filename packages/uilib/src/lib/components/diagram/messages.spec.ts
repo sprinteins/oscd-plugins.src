2d93510a92293490ffe6fe4b26603e225a7fc850
Tamás Russ <t.russ@sprinteins.com>
2023-04-28 16:12:32 +0200

    convert test to table-driven test

--- packages/uilib/src/lib/components/diagram/messages.spec.ts
+++ packages/uilib/src/lib/components/diagram/messages.spec.ts
@@ -7,52 +7,54 @@ import type { IEDConnection } from "./nodes"
 
 
 describe("Messages", () => {
-	it("renders", async () => {
 
-		const isSelected = false
+	type TestCase = {
+		desc: string,
+		isSelected: boolean,
+		isSelectedClassExpected: boolean,
+	}
+
+	const featureTests: TestCase[] = [
+		{
+			desc:                    "has selected class if isSelected is true",
+			isSelected:              true,
+			isSelectedClassExpected: true,
+		},
+		{
+			desc:                    "does not have selected class if isSelected is false",
+			isSelected:              false,
+			isSelectedClassExpected: false,
+		},
+	]
+
+	featureTests.forEach(testIsSelectedAttribute)
+
+	function testIsSelectedAttribute(tc: TestCase) {
+		it(tc.desc, () => {
 	
-		const connection: IEDConnection = {
-			id:       "not_relevant",
-			sources:  [],
-			targets:  [],
-			sections: [
-				{
-					id:         "connection",
-					startPoint: {x: 0, y: 0},
-					endPoint:   {x: 0, y: 200},
-					bendPoints: [],
-				},
-			],
-		}
-
-		render(Message, { edge: connection, isSelected })
-		const element = screen.getByTestId("connection")
-		expect(element).toBeDefined
-		expect(element.classList.contains("show-selected-path")).toBeFalsy()
-	})
-
-	it("renders", async () => {
-
-		const isSelected = true
+			const connection: IEDConnection = {
+				id:       "not_relevant",
+				sources:  [],
+				targets:  [],
+				sections: [
+					{
+						id:         "connection",
+						startPoint: {x: 0, y: 0},
+						endPoint:   {x: 0, y: 200},
+						bendPoints: [],
+					},
+				],
+			}
 	
-		const connection: IEDConnection = {
-			id:       "not_relevant",
-			sources:  [],
-			targets:  [],
-			sections: [
-				{
-					id:         "connection",
-					startPoint: {x: 0, y: 0},
-					endPoint:   {x: 0, y: 200},
-					bendPoints: [],
-				},
-			],
-		}
-
-		render(Message, { edge: connection, isSelected })
-		const element = screen.getByTestId("connection")
-		expect(element).toBeDefined
-		expect(Array.from(element.classList)).toContain("show-selected-path")
-	})
+			render(Message, { edge: connection, isSelected: tc.isSelected })
+			const element = screen.getByTestId("connection")
+			expect(element).toBeDefined
+			if(tc.isSelectedClassExpected) {
+				expect(Array.from(element.classList)).toContain("selected")
+			} else {
+				expect(Array.from(element.classList)).not.toContain("selected")
+			}
+		})
+	}
 
 })
