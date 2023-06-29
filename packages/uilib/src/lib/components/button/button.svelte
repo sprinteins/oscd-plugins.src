11e5ee529cded5c0a50b88ca1aa01b1f72f06e34
r.schmid <r.schmid@sprinteins.com>
2023-06-29 14:23:00 +0200

    fixed little bug

--- packages/uilib/src/lib/components/button/button.svelte
+++ packages/uilib/src/lib/components/button/button.svelte
@@ -38,7 +38,7 @@
     height: auto;
     padding: var(--padding) calc(var(--padding) * 2);
   }
-  :global(button.mdc-button:not(:disabled)) {
-    color: var(--font-color);
+  :global(button.mdc-button) {
+    color: var(--font-color) !important;
   }
 </style>
